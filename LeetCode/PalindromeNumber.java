3cdf62d7bfd3973aac1f96d53a37277642cb7aed
anurag6063 <anurag6063@users.noreply.github.com>
2023-09-22 08:52:23 +0530

    Update PalindromeNumber.java
    own code

--- LeetCode/PalindromeNumber.java
+++ LeetCode/PalindromeNumber.java
@@ -31,3 +31,36 @@ class Solution {
         return (x==check || x==check/10);
 }
 }
+
+
+// -------------------------
+// own solution
+// first basic check
+// reverse whole no and then check.
+// optimization, check in b/w
+
+class Solution {
+  public boolean isPalindrome(int x) {
+
+      if( x < 0){
+          return false;
+      }
+      if(x/10 == 0 || x<0){
+          return true;
+      }
+    int num = x;
+    int rev = 0;
+
+      while(x>0){
+         rev = x % 10  + rev*10;
+         x = x/10;
+      }
+      
+    if(rev == num){
+        return true;
+    }
+   
+    return false;
+   
+  }
+}
