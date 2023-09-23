83beffe774aa16f87261b5c570fcbf14dec92156
anurag6063 <anurag6063@users.noreply.github.com>
2023-09-23 07:39:58 +0530

    Update ValidAnagram.java
    own code

--- LeetCode/ValidAnagram.java
+++ LeetCode/ValidAnagram.java
@@ -7,6 +7,66 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 
  */
 
+// own code
+
+// using char array counter
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        int size = 'z'-'a'+1;
+        int[] note = new int[size];
+
+        for(int i=0; i< sArr.length; i++){
+            // System.out.println(sArr[i]-'a');
+            note[sArr[i]-'a'] = note[sArr[i]-'a']+1;
+        }
+        for(int i=0; i< tArr.length; i++){
+            note[tArr[i]-'a'] = note[tArr[i]-'a']-1;
+        }
+        for(int i=0; i< note.length; i++){
+            if(note[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+// Using sorting
+/*class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        String sA = new String(sArr);
+        String tA = new String(tArr);
+        if(sA.toString().equalsIgnoreCase(tA.toString())){
+            return true;
+        }
+        return false;
+    }
+}
+*/
+
+// -------------------------
+
 // assumption: that all have valid alphabets with no special characters. 
 // add and minus from one array. 
 // at last check for any element still having non 0. 
