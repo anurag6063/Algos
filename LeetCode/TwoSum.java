dd9d046427b82e0d7961e5b34bebcb5ba269c79b
anurag6063 <anurag6063@users.noreply.github.com>
2023-09-23 07:14:36 +0530

    Update TwoSum.java
    own code

--- LeetCode/TwoSum.java
+++ LeetCode/TwoSum.java
@@ -10,6 +10,50 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
  */
 
+// own
+
+// optimized
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int[] result = new int[2];
+        Map<Integer, Integer> hm = new HashMap<>();
+        for(int i=0; i< nums.length; i++){
+            
+            int find = target - nums[i];
+            
+                if(hm.containsKey(find)){
+                    result[0]= i;
+                    result[1] = hm.get(find);
+                }
+             hm.put(nums[i], i);
+        }
+    return result;    
+    }
+}
+
+
+// brute force
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int[] result = new int[2];
+
+        for(int i=0; i< nums.length; i++){
+            for(int j= i+1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    result[0]= i;
+                    result[1] = j;
+                }
+            }
+        }
+
+    return result;    
+
+    }
+}
+
+// ---------------------------
 class Solution {
 
 public int[] twoSum(int[] nums, int target) {    
