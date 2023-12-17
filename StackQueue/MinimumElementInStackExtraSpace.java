c3883a896dfbc558c4fe9fb4c71fb04cbea7258f
anura <anurag.yadav@in.pega.com>
2023-12-17 08:29:02 +0530

    stacks

--- StackQueue/MinimumElementInStackExtraSpace.java
+++ StackQueue/MinimumElementInStackExtraSpace.java
@@ -14,3 +14,51 @@
 	
 
 */
+
+class MinStack {
+    Stack<Integer> minStack;
+    Stack<Integer> min;
+    public MinStack() {
+       minStack = new Stack<>();
+       min = new Stack<>();
+    }
+    
+    public void push(int val) {
+        minStack.push(val);
+        // if(min.isEmpty()) min.push(val);
+
+        if(min.isEmpty() || val <= min.peek()){
+            min.push(val);
+        }
+        // if(min.size()>0){
+        //     System.out.println(min.peek() + " MIN " +min.size());
+        // }
+        // System.out.println(minStack.peek() + " "+ minStack.size());
+    }
+
+    public void pop() {
+    if (minStack.peek().equals(min.peek())) {
+        min.pop();
+    }
+    minStack.pop();
+
+}
+
+    public int top() {
+       return minStack.peek();
+    }
+    
+    public int getMin() {
+        // return !min.isEmpty()? min.peek(): -1;
+        return min.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
