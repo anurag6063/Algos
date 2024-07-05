24ecc0c3d18c053902f9992e0bcefd0bfc34eff4
Anurag <anurag.a.yadav@oracle.com>
2024-07-05 07:21:46 +0530

    DP and trees practice

--- Trees/InOrder.java
+++ Trees/InOrder.java
@@ -16,13 +16,15 @@
 */
 // In-order traversal means to "visit" (often, print) the left branch, then the current node, and finally, the right
 // branch.
+
+/*
 void inOrderTraversal(TreeNode node) {
      if (node!= null) {
      inOrderTraversal(node.left);
      visit(node);
      inOrderTraversal(node.right);
      }
-7 }
+}
 
 // When performed on a binary search tree, it visits the nodes in ascending order (hence the name "in-order"). 
 // since we had to just return void the whole recursive function has been put in if 
@@ -40,7 +42,7 @@ class TreeNode{
 }
 
 
-public class Main
+public class InOrderTraversal
 {
 	public static void main(String[] args) {
 		System.out.println("Start--");
@@ -58,7 +60,7 @@ public class Main
         root.left.left = new TreeNode(4);
         root.left.right = new TreeNode(5);
         
-        Main main = new Main();
+        InOrderTraversal main = new InOrderTraversal();
         main.inOrderTraversal(root);
 	}
 	
@@ -74,3 +76,5 @@ public class Main
 	    // Output should be: 4 2 5 1 3
 	}
 }
+
+*/
