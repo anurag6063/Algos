45f3c5642fdbfbd0a84a3e7be747774642da5e73
Anurag <anurag.a.yadav@oracle.com>
2024-12-18 07:40:22 +0530

    DP problems practice

--- BasicsDSASheet/src/main/java/org/example/dp/JumpingFrog.java
+++ BasicsDSASheet/src/main/java/org/example/dp/JumpingFrog.java
@@ -38,6 +38,35 @@ public class JumpingFrog {
     return dp[index];
   }
 
+  private static int solveTabulation(int index, int[] height, int[] dp){
+    dp[0] = 0; // I am just using simple 1D array for tabulation.
+    for(int i = 1; i < height.length; i++){
+      int jumpTwo = Integer.MAX_VALUE;
+      int jumpOne = dp[i-1] + Math.abs(height[i] - height[i-1]);
+      if(i > 1){
+        jumpTwo = dp[i-2] + Math.abs(height[i] - height[i-2]);
+      }
+      dp[i] = Math.min(jumpOne, jumpTwo);
+    }
+    return dp[height.length-1];
+  }
+
+
+  private static int solveTabulationWithLessSpace(int index, int[] height){
+    int prev1 = 0;
+    int prev2 = 0;
+    for(int i = 1; i < height.length; i++){
+      int jumpTwo = Integer.MAX_VALUE;
+      int jumpOne = prev1 + Math.abs(height[i] - height[i-1]);
+      if(i > 1){
+        jumpTwo = prev2 + Math.abs(height[i] - height[i-2]);
+      }
+      int curr = Math.min(jumpOne, jumpTwo);
+      prev2 = prev1;
+      prev1 = curr;
+    }
+    return prev1;
+  }
   public static void main(String[] args) {
     int[] height = {30,10,60,10,60,50};
     int n = height.length;
